4023f1720cf9c12052671cf60d45e3f8931e7dd0
McClain Looney <mcclain@curationexperts.com>
2021-10-27 15:25:16 -0500

    Installs GoodJob

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "cur_#{Rails.env}"
+  config.active_job.queue_adapter = :good_job
 
   config.action_mailer.perform_caching = false
 
