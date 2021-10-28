9ba54b4c6540f67a1127180cc9c0d6837ce07ae8
McClain Looney <mcclain@curationexperts.com>
2021-10-28 12:21:22 -0500

    Ports over extension patches & specs

--- config/initializers/remonkey.rb
+++ config/initializers/remonkey.rb
@@ -5,8 +5,8 @@ ActiveSupport::Reloader.to_prepare do
   # Extend the hyrax classes here, so this gets called on reload
   # to re-patch the newly redefined classes
   # TODO: uncomment these when they've been imported
-  # Hyrax::MenuPresenter.include(Extensions::MenuPresenter)
-  # Hyrax::FileSetDerivativesService.include(Extensions::PtiffDerivative)
-  # Hydra::Derivatives::Processors::Image.include(Extensions::ImageProcessor)
-  # Hyrax::DerivativePath.include(Extensions::DerivativePath)
+  Hyrax::MenuPresenter.include(Extensions::MenuPresenter)
+  Hyrax::FileSetDerivativesService.include(Extensions::PtiffDerivative)
+  Hydra::Derivatives::Processors::Image.include(Extensions::ImageProcessor)
+  Hyrax::DerivativePath.include(Extensions::DerivativePath)
 end
