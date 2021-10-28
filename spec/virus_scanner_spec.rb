3e3320e3ef901c1396cc24ec29ea4e8b3f6b0191
McClain Looney <mcclain@curationexperts.com>
2021-10-28 15:14:19 -0500

    Adds ClamAV virus scanner support

---
+++ spec/virus_scanner_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require './app/lib/virus_scanner'
+
+RSpec.describe Cur::VirusScanner do
+  context "with clean file" do
+    let(:goodfile){'./spec/fixtures/images/cat.jpg'}
+    it "should not freak out" do
+      expect(described_class.new(goodfile).infected?).to be false
+    end
+  end
+  context "with nonexistent file" do
+    let(:badfile){'./nothere'}
+    it "should raise " do
+      expect{described_class.new(badfile).infected?}.to raise_error  Clamby::FileNotFound
+    end
+  end
+  context "with virus file" do
+    let(:badfile){'./spec/fixtures/virus_check.txt'}
+    it "should freak out" do
+      expect(described_class.new(badfile).infected?).to be_truthy
+    end
+  end
+end
