198d3ebb905f40bba2f0fba38ee8e0f70d13ab4b
Norm Orstad <norman@orstad.com>
2021-12-09 16:25:44 -0600

    Homepage initial layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def get_random_image
+    image_path_prefix = "app/assets/images/"
+    image_files = Dir.glob("#{image_path_prefix}unsplash/*")
+    image_files.sample.split(image_path_prefix)[1]
+  end
 end
