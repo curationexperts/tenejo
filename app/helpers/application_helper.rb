d2506d2d8187c51276d809e6d18d47b5ed419187
McClain Looney <mcclain@curationexperts.com>
2021-12-14 11:14:07 -0600

    Repairs specs & rubocop complaints

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,7 +21,7 @@ module ApplicationHelper
     rgb[0] = (rgb[0].to_i * amount).round
     rgb[1] = (rgb[1].to_i * amount).round
     rgb[2] = (rgb[2].to_i * amount).round
-    "#%02x%02x%02x" % rgb
+    format("#%02x%02x%02x", *rgb)
   end
 
   # Amount should be a decimal between 0 and 1. Higher means lighter
@@ -31,7 +31,7 @@ module ApplicationHelper
     rgb[0] = [(rgb[0].to_i + 255 * amount).round, 255].min
     rgb[1] = [(rgb[1].to_i + 255 * amount).round, 255].min
     rgb[2] = [(rgb[2].to_i + 255 * amount).round, 255].min
-    "#%02x%02x%02x" % rgb
+    format("#%02x%02x%02x",  *rgb)
   end
 
   # Defines text colors, white or black, depending on the brightness value
