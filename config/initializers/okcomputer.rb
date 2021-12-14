044b17ed402900a57fe574d94841f843761be6a0
McClain Looney <mcclain@curationexperts.com>
2021-12-14 10:54:05 -0600

    added clamav checks & renamed tomcat check

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -25,5 +25,7 @@ end
 OkComputer::Registry.register "redis", ServiceCheck.new('redis-server')
 OkComputer::Registry.register "solr", HttpCheck.new(ENV['SOLR_URL'])
 OkComputer::Registry.register "fedora", HttpCheck.new(ENV['FEDORA_URL'])
-OkComputer::Registry.register "tomcat", ServiceCheck.new('tomcat9')
+OkComputer::Registry.register "vips", ServiceCheck.new('tomcat9')
 OkComputer::Registry.register "postgres", ServiceCheck.new('postgresql')
+OkComputer::Registry.register "antivirus service", ServiceCheck.new('clamav-daemon')
+OkComputer::Registry.register "antivirus updates", ServiceCheck.new('clamav-freshclam')
