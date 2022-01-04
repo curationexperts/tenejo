21af0d12c15ac3184d43385446720722500b21dc
McClain Looney <mlooney@users.noreply.github.com>
2022-01-04 14:03:59 -0600

    Adds support for selecting roles when creating new users (#72)
    Co-authored-by: McClain Looney <mcclain@curationexperts.com>

---
+++ app/controllers/tenejo/invite_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+class Tenejo::InviteController < Devise::InvitationsController
+  before_action :configure_permitted_parameters
+
+  protected
+
+  # form was modified to include new role_ids param, so we have to tell devise about it
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:invite) do |pr|
+      pr.permit(:email, role_ids: [])
+    end
+  end
+end
---
+++ spec/requests/invite_request_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe "/tenejo/invite", type: :request do
+  context "not logged in" do
+    it "redirects" do
+      get new_user_invitation_path
+      expect(response).to redirect_to new_user_session_path
+    end
+  end
+  context "logged in as admin" do
+    let(:admin) { User.create(email: 'test@example.com', password: '123456', roles: [Role.create(name: 'admin')]) }
+    let(:newuser) { User.new(email: 'newuser@example.com', password: '123456', role_ids: [Role.create(name: 'test').id]) }
+    before do
+      sign_in admin
+    end
+
+    describe "GET /index" do
+      it "renders a successful response" do
+        get new_user_invitation_path
+        expect(response).to be_successful
+        expect(assigns(:user)).not_to be_nil
+        expect(response).to render_template('devise/invitations/new')
+      end
+    end
+    describe "POST /create" do
+      it "renders a successful response" do
+        post user_invitation_path, params: { user: { email: 'newuser@example.com', password: '123456', role_ids: [admin.roles.first.id] } }
+        expect(assigns(:user)).not_to be_nil
+        expect(response).to redirect_to dashboard_path
+        expect(User.last.roles.map(&:id)).to eq [admin.roles.first.id]
+        expect(flash[:notice]).to eq "An invitation email has been sent to newuser@example.com."
+      end
+    end
+  end
+end
