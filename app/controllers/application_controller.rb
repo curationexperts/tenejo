5fdd199714f770c061ace41dbdf7373941b13325
McClain Looney <mcclain@curationexperts.com>
2022-01-12 14:05:12 -0600

    Includes all Hyrax engine helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   rescue_from Net::SMTPFatalError, with: :mail_error
+  helper Hyrax::Engine.helpers
   helper Openseadragon::OpenseadragonHelper
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
