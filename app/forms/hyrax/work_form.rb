5c47b4bdd4133c0e2f17f26d90cd16f6fb54f43f
McClain Looney <mcclain@curationexperts.com>
2022-01-26 12:35:14 -0600

    Fix field name

--- app/forms/hyrax/work_form.rb
+++ app/forms/hyrax/work_form.rb
@@ -6,7 +6,7 @@ module Hyrax
   # Generated form for Work
   class WorkForm < Hyrax::Forms::WorkForm
     self.model_class = ::Work
-    self.required_terms += [:primary_identifier]
+    self.required_fields += [:primary_identifier]
     self.terms += [:resource_type]
   end
 end
