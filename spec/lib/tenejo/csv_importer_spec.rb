1dfbd60d1dd118639259ad42f0d19d1f29ba0ad7
Mark Bussey <mark@curationexperts.com>
2022-01-28 10:40:39 -0800

    Split out spec for testing work and file nesting in CSVs

--- spec/lib/tenejo/csv_importer_spec.rb
+++ spec/lib/tenejo/csv_importer_spec.rb
@@ -41,42 +41,6 @@ RSpec.describe Tenejo::CsvImporter do
     expect(csv_import).to have_received(:create_or_update_file).exactly(31).times
   end
 
-  context 'with a valid CSV' do
-    let(:csv) { fixture_file_upload("./spec/fixtures/csv/structure_test.csv") }
-    it 'builds the expected objects and relationships', :aggregate_failures do
-      ActiveFedora::Cleaner.clean!
-      described_class.reset_default_collection_type!
-      csv_import = described_class.new(import_job)
-      csv_import.import
-
-      parent = Collection.where(primary_identifier: 'EPHEM').first
-      child = Collection.where(primary_identifier: 'CARDS').first
-      grandchild = Work.where(primary_identifier: 'CARDS-0001').find { |w| w.identifier == ['CARDS-0001'] }
-      greatgrandchild = Work.where(primary_identifier: 'CARDS-0001-J').find { |w| w.identifier == ['CARDS-0001-J'] }
-
-      expect(parent.child_collections).to include child
-      expect(child.parent_collections).to include parent
-      expect(child.child_collections).to be_empty
-      expect(child.child_works).to include grandchild
-      expect(greatgrandchild.parent_works).to include grandchild
-
-      private_work = Work.where(primary_identifier: 'ORPH-0001').find { |w| w.identifier == ['ORPH-0001'] }
-      institutional_work = Work.where(primary_identifier: 'ORPH-0002').find { |w| w.identifier == ['ORPH-0002'] }
-      public_work = Work.where(primary_identifier: 'CARDS-0001-J').find { |w| w.identifier == ['CARDS-0001-J'] }
-
-      expect(private_work.visibility).to eq Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE
-      expect(institutional_work.visibility).to eq Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED
-      expect(public_work.visibility).to eq Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
-
-      private_collection = Collection.where(primary_identifier: 'DARK').first
-      public_collection = Collection.where(primary_identifier: 'CARDS').find { |w| w.identifier == ['CARDS'] }
-
-      expect(private_collection.visibility).to eq Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE
-      expect(public_collection.visibility).to eq Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
-
-      expect(Work.where(primary_identifier: 'CARDS-0001').count).to be > 1, "remember to simplify these test when identifier is refactored"
-    end
-  end
 
   context '.create_or_update_collection' do
     before { allow(Tenejo::Preflight).to receive(:process_csv) } # skip creating the preflight graph
