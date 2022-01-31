b95c7d3f203eeb64347966a8746c72ceed6613a9
Mark Bussey <mark@curationexperts.com>
2022-01-31 09:01:40 -0800

    Break single large test into smaller tests

--- spec/lib/tenejo/nesting_csv_importer_spec.rb
+++ spec/lib/tenejo/nesting_csv_importer_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Tenejo::CsvImporter do
   before :all do
     ActiveFedora::Cleaner.clean!
     described_class.reset_default_collection_type!
-    job_owner = User.first_or_create!(FactoryBot.attributes_for(:user))
+    job_owner = User.find_by(email: 'admin@example.org') || User.create(email: 'admin@example.org', password: 'abcd5678')
     csv = fixture_file_upload("./spec/fixtures/csv/structure_test.csv")
     preflight = Preflight.create!(user: job_owner, manifest: csv)
     import_job = Import.create!(user: job_owner, parent_job: preflight)
@@ -15,34 +15,35 @@ RSpec.describe Tenejo::CsvImporter do
     csv_import.import
   end
 
-  context 'with a valid CSV' do
-    it 'builds the expected objects and relationships', :aggregate_failures do
-      parent = Collection.where(primary_identifier: 'EPHEM').first
-      child = Collection.where(primary_identifier: 'CARDS').first
-      grandchild = Work.where(primary_identifier: 'CARDS-0001').find { |w| w.identifier == ['CARDS-0001'] }
-      greatgrandchild = Work.where(primary_identifier: 'CARDS-0001-J').find { |w| w.identifier == ['CARDS-0001-J'] }
+  it 'builds relationships between parents and children', :aggregate_failures do
+    parent = Collection.where(primary_identifier: 'EPHEM').first
+    child = Collection.where(primary_identifier: 'CARDS').first
+    grandchild = Work.where(primary_identifier: 'CARDS-0001').find { |w| w.identifier == ['CARDS-0001'] }
+    greatgrandchild = Work.where(primary_identifier: 'CARDS-0001-J').find { |w| w.identifier == ['CARDS-0001-J'] }
 
-      expect(parent.child_collections).to include child
-      expect(child.parent_collections).to include parent
-      expect(child.child_collections).to be_empty
-      expect(child.child_works).to include grandchild
-      expect(greatgrandchild.parent_works).to include grandchild
+    expect(parent.child_collections).to include child
+    expect(child.parent_collections).to include parent
+    expect(child.child_collections).to be_empty
+    expect(child.child_works).to include grandchild
+    expect(greatgrandchild.parent_works).to include grandchild
+  end
 
-      private_work = Work.where(primary_identifier: 'ORPH-0001').find { |w| w.identifier == ['ORPH-0001'] }
-      institutional_work = Work.where(primary_identifier: 'ORPH-0002').find { |w| w.identifier == ['ORPH-0002'] }
-      public_work = Work.where(primary_identifier: 'CARDS-0001-J').find { |w| w.identifier == ['CARDS-0001-J'] }
+  it 'sets visibility on works', :aggregate_failures do
+    private_work = Work.where(primary_identifier: 'ORPH-0001').find { |w| w.identifier == ['ORPH-0001'] }
+    institutional_work = Work.where(primary_identifier: 'ORPH-0002').find { |w| w.identifier == ['ORPH-0002'] }
+    public_work = Work.where(primary_identifier: 'CARDS-0001-J').find { |w| w.identifier == ['CARDS-0001-J'] }
 
-      expect(private_work.visibility).to eq Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE
-      expect(institutional_work.visibility).to eq Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED
-      expect(public_work.visibility).to eq Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
+    expect(private_work.visibility).to eq Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE
+    expect(institutional_work.visibility).to eq Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED
+    expect(public_work.visibility).to eq Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
+    expect(Work.where(primary_identifier: 'CARDS-0001').count).to be > 1, "remember to simplify these test when identifier is refactored"
+  end
 
-      private_collection = Collection.where(primary_identifier: 'DARK').first
-      public_collection = Collection.where(primary_identifier: 'CARDS').find { |w| w.identifier == ['CARDS'] }
+  it 'sets visibility on collections', :aggregate_failures do
+    private_collection = Collection.where(primary_identifier: 'DARK').first
+    public_collection = Collection.where(primary_identifier: 'CARDS').find { |w| w.identifier == ['CARDS'] }
 
-      expect(private_collection.visibility).to eq Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE
-      expect(public_collection.visibility).to eq Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
-
-      expect(Work.where(primary_identifier: 'CARDS-0001').count).to be > 1, "remember to simplify these test when identifier is refactored"
-    end
+    expect(private_collection.visibility).to eq Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE
+    expect(public_collection.visibility).to eq Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
   end
 end
