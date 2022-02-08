51616157c01d2e5358371a4be5473eb046c1dcee
McClain Looney <mcclain@curationexperts.com>
2022-02-08 15:58:24 -0600

    Index into correct hash for role_ids

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
 
   def update
     @user.display_name = params[:user][:display_name]
-    @user.roles << Role.find(params[:role_ids])
+    @user.roles << Role.find(params[:user][:role_ids])
     @user.save!
     flash[:notice] = "User updated"
     redirect_to hyrax.admin_users_path
