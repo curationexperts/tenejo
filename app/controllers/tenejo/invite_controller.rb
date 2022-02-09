236bb100e2fecae7279542f647fd6585032b23c7
Norm Orstad <norman@orstad.com>
2022-02-09 17:11:27 -0600

    User invite uses dashboard theme

--- app/controllers/tenejo/invite_controller.rb
+++ app/controllers/tenejo/invite_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class Tenejo::InviteController < Devise::InvitationsController
   before_action :configure_permitted_parameters
-
+  with_themed_layout 'dashboard'
   protected
 
   # form was modified to include new role_ids param, so we have to tell devise about it
