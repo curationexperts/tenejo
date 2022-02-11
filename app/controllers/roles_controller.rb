aa09f9b69ec78ace1e275fa7846506fb97de6151
Norm Orstad <norm0@users.noreply.github.com>
2022-02-11 14:07:32 -0600

    Roles dashboard updates (#132)
    * Remove unused show page
    * Better confirmation messages
    * Redirects to roles list after removing a role from the list

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -4,4 +4,13 @@
 class RolesController < ApplicationController
   include Hydra::RoleManagement::RolesBehavior
   with_themed_layout 'dashboard'
+
+  def destroy
+    if @role.destroy
+      redirect_to hyrax.admin_users_path(anchor: "roles"),
+      notice: 'Role was successfully deleted.'
+    else
+      redirect_to hyrax.admin_users_path(anchor: "roles")
+    end
+  end
 end
