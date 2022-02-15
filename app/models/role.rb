2ac11952b89720fe53b687fc9f2425b482be6305
McClain Looney <mcclain@curationexperts.com>
2022-02-15 10:18:40 -0600

    Remove redundant non-documentation

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
-
-# Class modeling Roles within the application
 class Role < ApplicationRecord
   before_update :readonly!
   has_and_belongs_to_many :users # # rubocop:disable Rails/HasAndBelongsToMany
