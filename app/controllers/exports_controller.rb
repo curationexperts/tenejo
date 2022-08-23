28d6a51215065aa04d3ca10aa031d4d5baab2dbb
Norm Orstad <norm0@users.noreply.github.com>
2022-08-23 13:00:58 -0500

    Jobs Card Views (#181)
    * Adds card design to job show page
    
    * Adds card design to preflight
    
    * Add exports views with card design
    
    * Added export routes
    
    * Wrong file type name
    
    * Main header with title and action buttons aligned
    
    * Preflights with cleaner markup
    
    * Style updates to 'Jobs' index page
    
    * Style updates to 'Export' show page
    
    * Updates styles for jobs new
    
    * Removes breadcrumbs
    
    * Rubocop fixes
    
    * Cleanup tests for imports
    
    * Added conditionals to the object models in to the preflight, imports and exports
    
    * Cleaned up tests
    
    * Removes conditionals to the object models
    
    * Clean render attribute

---
+++ app/controllers/exports_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class ExportsController < JobsController
+  with_themed_layout 'dashboard'
+
+  def index
+    redirect_to jobs_path
+  end
+
+  def new
+    redirect_to new_export_path
+  end
+
+  def show
+  end
+end
