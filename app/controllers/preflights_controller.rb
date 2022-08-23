9a2964bacae089e013793b001945ad98a2b116cb
Norm Orstad <norman@orstad.com>
2022-08-23 13:16:44 -0500

    Rubocop fixes

--- app/controllers/preflights_controller.rb
+++ app/controllers/preflights_controller.rb
@@ -15,8 +15,7 @@ class PreflightsController < JobsController
   def show
     super
     @preflight_graph = Tenejo::Preflight.process_csv(@job.manifest.download)
-    add_breadcrumb (I18n.t('tenejo.admin.sidebar.preflight' ) + ' #' + params[:id].to_s), @job
- 
+    add_breadcrumb (I18n.t('tenejo.admin.sidebar.preflight') + ' #' + params[:id].to_s), @job
   end
 
   def create
