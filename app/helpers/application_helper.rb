304cbb38e0c74bb1e48f1d428518e77a309f853e
Norm Orstad <norm0@users.noreply.github.com>
2022-08-29 10:07:22 -0500

    Adds sorting, status pills, pagination to jobs index table (#189)
    * Adds sorting, status pills, pagination to jobs index table
    
    * Rubocop fixes
    
    * Refactor job status helper (#192)
    
    * Add basic tests for job status helper
    * Update deprecated tag syntax
    * Use dedicated status classes instead of overriding background classes
    * Organize helper to highlight differences between statuses
    * Update import show spec to reflect badge change
    
    Co-authored-by: Mark Bussey <mark@curationexperts.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -60,6 +60,26 @@ module ApplicationHelper
     users.all.collect { |u| [u.user_key, u.display_name] }
   end
 
+  def status_span_generator(status)
+    status_text = status.to_s.titleize
+    case status_text
+    when 'Submitted'
+      status_classes = 'status-submitted'
+    when 'Errored'
+      status_classes = 'status-errored'
+    when 'Completed'
+      status_classes = 'status-completed'
+    when 'In Progress'
+      status_classes = 'status-in-progress'
+    when 'Unknown'
+      status_classes = 'status-unknown'
+    else
+      status_classes = 'status-unrecognized'
+      status_text = 'Unrecognized Status'
+    end
+    tag.span(status_text, class: 'job-status badge rounded-pill ' + status_classes)
+  end
+
   def collection_permission_template_form_for(form:)
     case form
     when Valkyrie::ChangeSet
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -6,4 +6,18 @@ describe ApplicationHelper do
     expect(helper.random_image).not_to be_nil
     expect(helper.random_image).to match(/unsplash\/.*\.jpg/)
   end
+
+  context "#status_span_generator" do
+    example "for known statuses" do
+      expect(helper.status_span_generator('submitted')).to include "Submitted"
+    end
+
+    example "for unrecognized statuses" do
+      expect(helper.status_span_generator("it's complicated")).to include "Unrecognized"
+    end
+
+    example "accepts symbols" do
+      expect(helper.status_span_generator(:in_progress)).to include "In Progress"
+    end
+  end
 end
