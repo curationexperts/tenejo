fcb048256e0e6aba9b43b181d9946b75bd9e339b
McClain Looney <mcclain@curationexperts.com>
2022-09-06 13:06:44 -0500

    Rubocop

--- app/lib/ldp/response.rb
+++ app/lib/ldp/response.rb
@@ -1,7 +1,7 @@
+# frozen_string_literal: true
 require 'ldp/response'
 
 Ldp::Response.class_eval do
-  puts "Raised up"
   def content_disposition_filename
     filename = content_disposition_attributes['filename']
     CGI.unescape(filename) if filename
