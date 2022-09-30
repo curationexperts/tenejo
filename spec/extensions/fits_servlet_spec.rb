f3ead9138d552f88c1c055b3a2dac2a298c66760
McClain Looney <mcclain@curationexperts.com>
2022-09-30 10:06:28 -0500

    Adds a test for the fits service

---
+++ spec/extensions/fits_servlet_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe Hydra::FileCharacterization::Characterizers::FitsServlet do
+  if ENV['FITS_SERVLET_URL'] # don't run unless we have a fits env
+    it 'can handle files with commas' do
+      filename = "this, terrible, filename.txt"
+      FileUtils.touch filename
+      system(described_class.new(filename).send(:command), exception: true)
+      FileUtils.rm filename
+    end
+    it 'can handle files with quotes' do
+      filename = "this', terrible, 'filename.txt"
+      FileUtils.touch filename
+      system(described_class.new(filename).send(:command), exception: true)
+      FileUtils.rm filename
+    end
+    it 'can handle paths with quotes' do
+      dirname="la 'fool,"
+      filename = "ee'this', terrible, 'filename.txt"
+      FileUtils.mkdir dirname
+      FileUtils.touch filename
+      system(described_class.new(filename).send(:command), exception: true)
+      FileUtils.rm filename
+      FileUtils.rmdir dirname
+    end
+  end
+end
