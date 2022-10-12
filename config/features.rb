496b575eb0c4cbd9faa4809600b2d1d304836531
Norm Orstad <norman@orstad.com>
2022-10-12 12:08:13 -0500

    Adds feature flipper to footer for versioning and branch

---
+++ config/features.rb
@@ -0,0 +1,5 @@
+Flipflop.configure do
+  feature :footer_version,
+          default: false,
+          description: "Displays the footer versions. Including SHA, branch, latest deployment and updated date."
+end
